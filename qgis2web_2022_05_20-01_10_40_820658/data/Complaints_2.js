9654758066c664958ce1c980f3d4e130c1141410
Nathi Nono <105371162+nnxnat002@users.noreply.github.com>
2022-05-20 01:32:42 +0300

    Add files via upload

---
+++ qgis2web_2022_05_20-01_10_40_820658/data/Complaints_2.js
@@ -0,0 +1,17 @@
+var json_Complaints_2 = {
+"type": "FeatureCollection",
+"name": "Complaints_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Location": "Mowbray Railway", "Offence": "Metal Theft", "Suspect": "Warona VanZyl", "Action": "Arrested", "Date": "2022-05-05", "ComplainID": "360235" }, "geometry": { "type": "Point", "coordinates": [ 18.473913188556327, -33.947110160294429 ] } },
+{ "type": "Feature", "properties": { "Location": "UCT Bridge to Rugby Field", "Offence": "Graffitti", "Suspect": "Unknown", "Action": "No action", "Date": "2022-05-10", "ComplainID": "364515" }, "geometry": { "type": "Point", "coordinates": [ 18.463304348654972, -33.958554039038781 ] } },
+{ "type": "Feature", "properties": { "Location": "6 Selby Road, Mowbray", "Offence": "Noise", "Suspect": "Blessings Moses", "Action": "Warning", "Date": "2022-05-07", "ComplainID": "361548" }, "geometry": { "type": "Point", "coordinates": [ 18.475894668414959, -33.948730841792603 ] } },
+{ "type": "Feature", "properties": { "Location": "7 Strubens Road, Mowbray", "Offence": "Noise", "Suspect": "Tinashe Shaka", "Action": "Fine", "Date": "2022-05-07", "ComplainID": "361245" }, "geometry": { "type": "Point", "coordinates": [ 18.474657047406119, -33.94676843853447 ] } },
+{ "type": "Feature", "properties": { "Location": "7 Strubens  Road, Mowbray", "Offence": "Graffitti", "Suspect": "Unknown", "Action": "No action", "Date": "2022-05-13", "ComplainID": "364258" }, "geometry": { "type": "Point", "coordinates": [ 18.474688126796668, -33.946854178366301 ] } },
+{ "type": "Feature", "properties": { "Location": "Mowbray to Obsevatory Bridge", "Offence": "Graffitti", "Suspect": "Unkown", "Action": "No action", "Date": "2022-05-09", "ComplainID": "362659" }, "geometry": { "type": "Point", "coordinates": [ 18.469900123007076, -33.945526498674077 ] } },
+{ "type": "Feature", "properties": { "Location": "Green Pass, Observatory", "Offence": "Graffitti", "Suspect": "Michael Smith", "Action": "Community servise", "Date": "2022-05-02", "ComplainID": "362587" }, "geometry": { "type": "Point", "coordinates": [ 18.468132345702593, -33.943912092800659 ] } },
+{ "type": "Feature", "properties": { "Location": "E2A-Arcade, Mowbray", "Offence": "Noise", "Suspect": "Lebohang Bngadesh", "Action": "Fine", "Date": "2022-05-15", "ComplainID": "362548" }, "geometry": { "type": "Point", "coordinates": [ 18.471540004580454, -33.948668089050429 ] } },
+{ "type": "Feature", "properties": { "Location": "Rosebank Rilway", "Offence": "Metal Theft", "Suspect": "Unknown", "Action": "No action", "Date": "2022-05-04", "ComplainID": "362597" }, "geometry": { "type": "Point", "coordinates": [ 18.473099007791596, -33.955335664866716 ] } },
+{ "type": "Feature", "properties": { "Location": "Rosebank Station", "Offence": "Metal Theft", "Suspect": "Senzo Cekiso", "Action": "Arrested", "Date": "2022-05-10", "ComplainID": "362157" }, "geometry": { "type": "Point", "coordinates": [ 18.473142536435738, -33.954683920264884 ] } }
+]
+}
---
+++ qgis2web_2022_05_20-01_10_40_820658/data/SouthernSuburb_1.js
@@ -0,0 +1,8 @@
+var json_SouthernSuburb_1 = {
+"type": "FeatureCollection",
+"name": "SouthernSuburb_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 18.463437552458963, -33.936780856065468 ], [ 18.483751738169765, -33.938123179306793 ], [ 18.49561666079731, -33.968990772285302 ], [ 18.45948257824978, -33.961535857665837 ], [ 18.463437552458963, -33.936780856065468 ] ] ] ] } }
+]
+}
